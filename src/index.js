afd770591b0909ba62572c5090cfc145be7724d9
Michael <melezhik082@gmail.com>
2021-04-11 17:18:35 +0300

    removed unnecessary

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {Router, Route, Switch } from "react-router-dom";
-import ItemCard from './components/itemCard/ItemCard';
 import { history } from './components/history';
 import List from './components/list/List';
 import Basket from './components/cart/Cart';
@@ -15,7 +14,6 @@ ReactDOM.render(
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/list" component={List} />
-        <Route path="/itemCard" component={ItemCard} />
         <Route path="/basket" component={Basket} />
         <Route path="/header" component={Header} />
       </Switch>
